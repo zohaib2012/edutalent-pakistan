76f315c6614f3f7a0cc559d2edca280c38d2763d
zohaib khalid <zohaibchaudry60@gmail.com>
2026-07-16 13:34:12 +0500

    Fix API path handling: normalize req.url to include /api prefix for Express routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const app = require('../server/src/app');
 
 module.exports = (req, res) => {
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + req.url;
+  }
   return app(req, res);
 };
